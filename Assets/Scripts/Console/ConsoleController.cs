16321f3b4749eb060deda02d6995ea230bf8ca3d
Ryan <ryan@glorifiedstudios.com>
2022-07-24 01:54:54 +0200

    better activation principle

--- Assets/Scripts/Console/ConsoleController.cs
+++ Assets/Scripts/Console/ConsoleController.cs
@@ -99,11 +99,15 @@ namespace Protomod
 
         private void Update()
         {
-            if( !consoleActive && Input.GetKeyDown( KeyCode.BackQuote ) )
+            if( Input.GetKeyDown( KeyCode.F1 ) )
+                ToggleConsole();
+
+            // While the below code is very useful, Unity automatically makes the crosshair visible when you press Escape, so this should be used in the future/in 2D games.
+            /*if( !consoleActive && Input.GetKeyDown( KeyCode.BackQuote ) )
                 ToggleConsole();
 
             if( consoleActive && Input.GetKeyDown( KeyCode.Escape ) )
-                ToggleConsole();
+                ToggleConsole();*/
         }
 
         // Drawing
