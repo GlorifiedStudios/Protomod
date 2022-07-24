a8d5ede83ebf9bd866151e4e2c49dc3c8ded2f42
Ryan <ryan@glorifiedstudios.com>
2022-07-25 00:57:12 +0200

    note

--- Mods/Test/test.lua
+++ Mods/Test/test.lua
@@ -3,4 +3,4 @@ local function OnModsLoaded()
     print( "All of our mods have been loaded!" )
 end
 
-Event.Register( "ModsLoaded", OnModsLoaded )
+Event.Register( "ModsLoaded", OnModsLoaded ) -- If we want to unregister this event, we can assign the expression to a variable and then call Event.Unregister()
